0a821e26e7cee5a665a0f712fad197a2405d0266
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-19 18:35:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {useState} from 'react'
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = event => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (inputValue !== '') {
+      setItems([...items, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDelete = index => {
+    setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul >
+        {items.map((item, index) => (
+          
+          <li key={index} style={{listStyleType:'none'}}>
+            {item}
+            <button style={{marginLeft:'15rem'}}onClick={() => handleDelete(index)}>Delete</button>
+          
+          </li>
+          
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
